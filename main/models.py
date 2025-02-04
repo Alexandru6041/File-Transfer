e3dab89a1c4707c6ed611d56ccbd31b7a7d7cffc
Chelariu Alexandru <alexchelariu834@gmail.com>
2025-02-04 10:04:27 +0200

    database updated

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class FileUnit(models.Model):
     IP = models.CharField(max_length=15)
     File = models.CharField(max_length=128)
     token = models.CharField(max_length=512)
+    server_ip = models.CharField(max_length=15)
     
     def __str__(self):
         return self.token
