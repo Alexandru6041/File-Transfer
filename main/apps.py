458a6b499ee03bbce7e0c0720d7e851e8c20ff7d
Chelariu Alexandru <alexchelariu834@gmail.com>
2025-02-04 09:59:31 +0200

    added logging capabilites in /log/main.log

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig
+from django.conf import settings
 
+#Third-Party
+from utils.network_utils.main import NetworkUtils
+import logging
+import os
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    
+    def ready(self):
+        if not logging.getLogger().hasHandlers():
+            
+            logging.basicConfig(filename="{}/main.log".format(settings.LOG_URL), 
+                                encoding = 'utf-8', 
+                                level = logging.DEBUG,
+                                format='%(asctime)s - %(levelname)s - %(message)s')
+            debug_handler = logging.FileHandler(os.path.join(settings.LOG_URL, 'main.log'), encoding='utf-8')
+            debug_handler.setLevel(logging.DEBUG)
+        
+        NetworkUtils().checkDatabase()
+        
